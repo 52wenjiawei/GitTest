221f04f63a15d6a685d66030aa4d1a8b2b51b210
52wenjiawei <164887123+52wenjiawei@users.noreply.github.com>
2024-03-26 21:14:33 +0800

    Update git.java

--- src/git.java
+++ src/git.java
@@ -3,5 +3,6 @@ public class git {
             System.out.println("I love you!");
             System.out.println("You are the best!");
             System.out.println("It is so difficult!!!");
+            System.out.println("真的是难死我算啦！");
         }
 }
