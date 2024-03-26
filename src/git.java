6ac5db9cd6183da0c4f1448ff8f8f1fd7b7eafc8
Susie <3312321174@qq.com>
2024-03-26 20:36:32 +0800

    second commit

--- src/git.java
+++ src/git.java
@@ -1,5 +1,6 @@
 public class git {
         public static void main(String[] args){
             System.out.println("I love you!");
+            System.out.println("You are the best!");
         }
 }
