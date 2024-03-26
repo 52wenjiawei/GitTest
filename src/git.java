252f157eef406994a39304fa58f1a62340bf7d17
Susie <3312321174@qq.com>
2024-03-26 20:39:48 +0800

    hot-fix commit

--- src/git.java
+++ src/git.java
@@ -2,5 +2,6 @@ public class git {
         public static void main(String[] args){
             System.out.println("I love you!");
             System.out.println("You are the best!");
+            System.out.println("It is so difficult!!!");
         }
 }
